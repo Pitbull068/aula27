6c46401a1ed84886524403cc052d55c9b39e50c3
guilherme <guilherme.p.carvalho6@aluno.senai.br>
2024-12-02 09:37:09 -0500

    revisao

---
+++ revisao.py
@@ -0,0 +1,12 @@
+#LER ENTRADAS DO USUÁRIO
+#nome = input()#ARMAZENAR o nome do aluno
+#print(nome)
+nota1 = float(input())# 4 notas do aluno
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+#faltas = int(input())
+#calculo da média
+media = (nota1+nota2+nota3+nota4)/4
+print(media)
