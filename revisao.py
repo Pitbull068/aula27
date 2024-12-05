eeee8b05f7ae339d1d399de0cb6fb21c1032aeb8
unknown <guilherme.p.carvalho6@aluno.senai.br>
2024-12-05 09:55:28 -0500

    para salvar menu

--- revisao.py
+++ revisao.py
@@ -1,35 +1,36 @@
 #LER ENTRADAS DO USUÁRIO
-cont = 0 #variavel que controla a repetição
-escolha_usuario = int(input()) #variavel que guarda quantas vezes o codigo vai rodar
-while cont < escolha_usuario:
-    nome = input()#ARMAZENAR o nome do aluno
-    nota1 = float(input())# 4 notas do aluno
-    nota2 = float(input())
-    nota3 = float(input())
-    nota4 = float(input())
+escolha_menu = int(input("escolha opção")) #Variavel que guarda qual opcao ele ta escolhendo
+alunos = []
+if escolha_menu == 1:#se a escolha for para realizar um cadastro
+    cont = 0 #variavel que controla a repetição
+    escolha_usuario = int(input("digite quantos alunos vão se cadastrar")) #variavel que guarda quantas vezes o codigo vai rodar
+    
+    while cont < escolha_usuario:
+        nome = input("digite o nome do aluno")#ARMAZENAR o nome do aluno
+        nota1 = float(input("digite nota1"))# 4 notas do aluno
+        nota2 = float(input("digite nota2"))
+        nota3 = float(input("digite nota3"))
+        nota4 = float(input("digite nota4"))
 
-    faltas = int(input())
-    #calculo da media
-    media = (nota1+nota2+nota3+nota4)/4
+        faltas = int(input("digite as faltas"))
+        #calculo da media
+        media = (nota1+nota2+nota3+nota4)/4
 
-    #logica da situação
-    if faltas >31:
-        situacao = "reprovado por falta"
-    elif media >=8:
-        situacao = "aprovado"
-    elif media >=5:#recuperação
-        recuperacao = float(input())#ler a nota da recuperação
-        if recuperacao >= (10-media):
-            situacao = "aprovado na recuperação"
+        #logica da situação
+        if faltas >31:
+            situacao = "reprovado por falta"
+        elif media >=8:
+            situacao = "aprovado"
+        elif media >=5:#recuperação
+            recuperacao = float(input("Digite nota"))#ler a nota da recuperação
+            if recuperacao >= (10-media):
+                situacao = "aprovado na recuperação"
+            else:
+                situacao = "reprovado na recuperação"
         else:
-            situacao = "reprovado na recuperação"
-    else:
-        situacao = "reprovado por media"
-
-
+            situacao = "reprovado por media"
+            #enviar os dados do aluno para a lista (aluno)
+        alunos.append([nome,faltas,media,situacao])
+        cont +=1
         #relatorio
-    print("nome",nome)
-    print("notas:",nota1,nota2,nota3,nota4)
-    print("nome",faltas)
-    print("nome",situacao)
-    cont = cont +1
+print(alunos)
